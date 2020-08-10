74c1eb0d35e848e5d0d4ff59287a787d7a395db2
Ajay Muralidharan <15915423+ajaymur91@users.noreply.github.com>
2020-08-10 16:24:00 -0500

    Update run_rect.sh

--- run_rect/run_rect.sh
+++ run_rect/run_rect.sh
@@ -9,5 +9,5 @@
 
 #spack load gromacs@2019.4
 
-time mpirun -n 4 gmx_mpi mdrun -deffnm md -cpi md.cpt -plumed ../plumed_rect.dat -multidir topol0 topol1 topol2 topol3 -replex 500 -nsteps 500000000 -hrex -pin on -maxh 48 -ntomp 1
+time mpirun -n 4 gmx_mpi mdrun -deffnm md -cpi md.cpt -plumed ../plumed_rect.dat -multidir topol0 topol1 topol2 topol3 -replex 500 -nsteps 500000000 -pin on -maxh 48 -ntomp 1
 
